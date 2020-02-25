2b18219b8e40e09b5c9393afe950d239a2dec199
xiangwangjianghu <a2894298377@163.com>
2020-02-25 11:36:41 +0800

    commit message

---
+++ readme.md
@@ -0,0 +1,17 @@
+# HBase Starter For SpringBoot
+
+## spring-boot-starter-hbase插件，集成HBase Client API 2.x修改的SpringBoot Starter组件
+
+* 实现获取Configuration
+* 实现获取Connection
+* 实现基本数据操作，如查询、保存等
+
+## 使用方法,引入Maven仓库:
+```
+<dependency>
+    <groupId>com.github.xiangwangjianghu</groupId>
+    <artifactId>spring-boot-starter-hbase</artifactId>
+    <version>1.0.0-RELEASE</version>
+</dependency>
+```
+# 承诺持续更新
---
+++ src/main/java/com/github/xiangwangjianghu/api/RowMapper.java
@@ -0,0 +1,24 @@
+package com.github.xiangwangjianghu.api;
+
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+
+/**
+ * Created by IntelliJ IDEA 2019.
+ * User: xiangwangjianghu.
+ * Date: 2019-05-17.
+ * Time: 10:53.
+ * To change this template use File Or Preferences | Settings | Editor | File and Code Templates.
+ * File Description: Callback for mapping rows of a {@link ResultScanner} on a per-row basis. Implementations of this interface perform the actual work of mapping each row to a result object, but don't need to worry about exception handling.
+ */
+public interface RowMapper<T> {
+    /**
+     * 回调函数，用于在每一行的基础上映射{@link ResultScanner}的行
+     *
+     * @param result
+     * @param rowNum
+     * @return
+     * @throws Throwable
+     */
+    T mapRow(Result result, int rowNum) throws Throwable;
+}
